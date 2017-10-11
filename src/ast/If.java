7c4bdc12a59ed0c3baafd9ade1615c49585c68db
CHRIS <chris.shandgost@gmail.com>
2017-10-11 15:56:21 +1100

    Initial commit

---
+++ src/ast/If.java
@@ -0,0 +1,25 @@
+package ast;
+
+import java.util.Set;
+
+public class If extends LCLExpression {
+
+	@Override
+	public Set<String> freeNames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LCLExpression substitute(String aVar, LCLExpression aExp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
